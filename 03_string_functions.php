bead0bcbb0115976a440bbac70d1eccec321fe7f
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-02-20 09:16:14 +0100

    Made 03_string_functions.php

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -4,8 +4,11 @@ $zin = "Binnenkort is het weeral lente";
 
 // Print af hoeveel woorden $zin bevat.
 
+echo str_word_count($zin, 0);
 
 // Vervang in $zin "lente" door "zomer" en druk af.
 
+echo "<br>";
+echo str_replace("lente", "zomer", $zin);
 
 ?>
