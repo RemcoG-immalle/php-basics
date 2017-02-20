a09ce1d3fcd1ac67aeb8370bec97fc468fcf0f23
RemcoG-immalle <remco.goyvaerts@immalle.be>
2017-02-20 09:48:11 +0100

    Fixed 06_arrays.php

--- 06_arrays.php
+++ 06_arrays.php
@@ -6,17 +6,20 @@ $names = array("Axel", "Sander", "Kenzo");
 
 // Itereer er over met een for-loop. Print ze af in een HTML ongenummerde lijst.
 
-for ($x = 0; $x <= count($names); $x++) {
-	echo $names[$x];
-	echo "<br>";
+echo "<ul>";
+for ($x = 0; $x < count($names); $x++) {
+	echo "<li>$names[$x]</li>";
 }
+echo "</ul>";
 
 // Itereer er over met een foreach-loop. Print ze af in HTML genummerde lijst.
 
+echo "<ol>";
 foreach ($names as $name) {
-	echo $name;
+	echo "<li>$name</li>";
 	echo "<br>";
 }
+echo "</ol>";
 
 echo "<br>";
 
